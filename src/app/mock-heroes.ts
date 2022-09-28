723ef2ddce10a253e32cad8a7f292c83102014b2
apatrikis <angelosp07@gmail.com>
2022-09-28 11:56:41 +0300

    Create Heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Prof. Chaos' },
+  { id: 3, name: 'OPM' },
+  { id: 4, name: 'Magma' },
+  { id: 5, name: 'Super Barista' },
+  { id: 6, name: 'Tatsumaki' },
+  { id: 7, name: 'Berserk' },
+  { id: 8, name: 'Dr. Nice' },
+  { id: 9, name: 'Dr. Not So Nice' },
+  { id: 10, name: 'Bombasto' },
+  { id: 11, name: 'Celeritas' }
+]
